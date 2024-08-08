c93f7a8fedf631e557fcd50c6d0c9a84b37856f4
Estevão Duarte <estevaoduarte.dev@gmail.com>
2024-08-08 19:00:43 -0300

    Adiciona comentários de explicação

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,20 +1,23 @@
+// 1 Desafio: Alterar tag HTML no JavaScript
 let titulo = document.querySelector('h1');
 
 titulo.innerHTML = 'Hora do Desafio!';
 
+// 2 Desafio: Exibir mensagem ao clicar em botão.
 function exibirMensagem() {
   console.log('O botão foi clicado');
 }
-
+// 3 Desafio: Exibir alert ao clicar em botão
 function exibirAlert() {
   alert('Eu amo JS');
 }
-
+// 4 Desafio: Chamar prompt ao clicar em botão e exibir mensagem do input prompt.
 function exibirPrompt() {
   let exibeCidade = prompt('Uma cidade do Brasil ?');
   return alert(`Estive em ${exibeCidade} e lembrei de você`);
 }
 
+// 5 Desafio: Exibir resultado de uma soma. Valores são escolhidos por usuário. 
 function exibirSoma() {
   let num1 = parseInt(prompt(`Entre com o primeiro valor`));
   let num2 = parseInt(prompt(`Entre com o segundo valor`));
--- js-curso-2-desafio_2/desafio.js
+++ js-curso-2-desafio_2/desafio.js
@@ -1,22 +1,24 @@
 //Desafio: Realizando atividades por meio de funções !! 
 
-// 1
+// 1 Função: Olá Mundo.
 function olaMundo() {
   console.log('Olá mundo');
 }
 olaMundo();
-// 2
+
+// 2 Função: Nome de uma pessoa.
 function olaPessoa(nome) {
   return nome;
 }
 olaPessoa('Jonas');
-// 3
+
+// 3 Função: O dobro de um número
 function dobroDoNumero(numero) {
   return numero * 2;
 }
 dobroDoNumero(34)
 
-// 4
+// 4 Função: A média da soma de 3 valores. 
 function maiorNumero(number1, number2, number3) {
   let media = (number1 + number2 + number3) / 3;
   return media;
@@ -25,7 +27,7 @@ function maiorNumero(number1, number2, number3) {
 let resultado = maiorNumero(11, 4, 17);
 console.log(`A média dos numéros é ${resultado}`)
 
-// 5
+// 5 Função: Quem é o maior entre os dois valores. 
 function oMaior(a, b) {
   let maior = a > b ? a : b;
   return maior;
@@ -34,12 +36,11 @@ function oMaior(a, b) {
 let numero = oMaior(23, 10);
 console.log(`O maior número é ${numero}`)
 
-// 6
-
+// 6 Função: Multiplicando um valor por ele mesmo.
 function multiplicaNumEntrada(a) {
   return (a * a);
 }
 
 let resultadoMultiplica = multiplicaNumEntrada(6);
 
-console.log(`O resultado da multiplicação da entrada por ela mesma é ${resultadoMultiplica}`);
+console.log(`Multiplicação de ${a} por ele mesmo é: ${resultadoMultiplica}`);
